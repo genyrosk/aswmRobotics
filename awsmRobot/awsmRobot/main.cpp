ab22ade19b3c8b99b0d61206efeb868e52434a4a
genyrosk <e.roskach@gmail.com>
2015-11-15 15:17:33 +0000

    first commit

---
+++ awsmRobot/awsmRobot/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  awsmRobot
+//
+//  Created by Evgeny Roskach on 09/11/15.
+//  Copyright © 2015 Awsme. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+    
+}
+
+
+class brain {
+    public
+};
